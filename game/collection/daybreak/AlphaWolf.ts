5e48141af5cf5bf4ec65b5fd2a24bc813cdec8d3
Kazuichi Takashiro <takashiro@qq.com>
2020-11-19 22:36:30 +0800

    Simplify Alpha Wolf

---
+++ game/collection/daybreak/AlphaWolf.ts
@@ -0,0 +1,7 @@
+import SinglePlayerSkill from '../SinglePlayerSkill';
+
+class AlphaWolf extends SinglePlayerSkill {
+	protected buttonLabel = '感染';
+}
+
+export default AlphaWolf;
--- game/collection/daybreak/index.ts
+++ game/collection/daybreak/index.ts
@@ -1,14 +1,16 @@
 import { Role } from '@bezier/werewolf-core';
 
+import Collection from '../Collection';
+
+import Werewolf from '../standard/Werewolf';
+import AlphaWolf from './AlphaWolf';
 import TwoPlayerSkill from '../TwoPlayerSkill';
 import WitchSkill from './WitchSkill';
-import AlphaWolfSkill from './AlphaWolfSkill';
 import MysticWolfSkill from './MysticWolfSkill';
-import Collection from '../Collection';
 
 const col = new Collection('daybreak');
 col.add(Role.Witch, WitchSkill);
-col.add(Role.AlphaWolf, AlphaWolfSkill);
+col.add(Role.AlphaWolf, Werewolf, AlphaWolf);
 col.add(Role.MysticWolf, MysticWolfSkill);
 col.add(Role.ParanormalInvestigator, TwoPlayerSkill);
 
