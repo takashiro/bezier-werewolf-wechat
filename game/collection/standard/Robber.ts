23bd88509371d7615a0f4118b0f8ee6479dcbd38
Kazuichi Takashiro <takashiro@qq.com>
2020-11-19 22:31:44 +0800

    Add class Robber

---
+++ game/collection/standard/Robber.ts
@@ -0,0 +1,5 @@
+import SinglePlayerSkill from '../SinglePlayerSkill';
+
+export default class Robber extends SinglePlayerSkill {
+	protected buttonLabel = '交换身份';
+}
--- game/collection/standard/index.ts
+++ game/collection/standard/index.ts
@@ -2,20 +2,20 @@ import { Role } from '@bezier/werewolf-core';
 import Collection from '../Collection';
 
 import TargetlessSkill from '../TargetlessSkill';
-import SinglePlayerSkill from '../SinglePlayerSkill';
 import SingleCardSkill from '../SingleCardSkill';
 
 import Werewolf from './Werewolf';
 import Seer from './Seer';
 import Minion from './Minion';
 import Troublemaker from './Troublemaker';
+import Robber from './Robber';
 
 const col = new Collection('standard');
 col.add(Role.Werewolf, Werewolf);
 col.add(Role.Seer, Seer);
 col.add(Role.Minion, Minion);
 col.add(Role.Troublemaker, Troublemaker);
-col.add(Role.Robber, SinglePlayerSkill);
+col.add(Role.Robber, Robber);
 col.add(Role.Drunk, SingleCardSkill);
 col.add(Role.Mason, TargetlessSkill);
 
