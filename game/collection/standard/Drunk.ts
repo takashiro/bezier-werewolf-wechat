3de0c1bca6544c040b781670af3169d5fec6e682
Kazuichi Takashiro <takashiro@qq.com>
2020-11-19 22:32:40 +0800

    Add class Drunk

---
+++ game/collection/standard/Drunk.ts
@@ -0,0 +1,5 @@
+import SingleCardSkill from '../SingleCardSkill';
+
+export default class Drunk extends SingleCardSkill {
+	protected buttonLabel = '交换身份';
+}
--- game/collection/standard/index.ts
+++ game/collection/standard/index.ts
@@ -2,13 +2,13 @@ import { Role } from '@bezier/werewolf-core';
 import Collection from '../Collection';
 
 import TargetlessSkill from '../TargetlessSkill';
-import SingleCardSkill from '../SingleCardSkill';
 
 import Werewolf from './Werewolf';
 import Seer from './Seer';
 import Minion from './Minion';
 import Troublemaker from './Troublemaker';
 import Robber from './Robber';
+import Drunk from './Drunk';
 
 const col = new Collection('standard');
 col.add(Role.Werewolf, Werewolf);
@@ -16,7 +16,7 @@ col.add(Role.Seer, Seer);
 col.add(Role.Minion, Minion);
 col.add(Role.Troublemaker, Troublemaker);
 col.add(Role.Robber, Robber);
-col.add(Role.Drunk, SingleCardSkill);
+col.add(Role.Drunk, Drunk);
 col.add(Role.Mason, TargetlessSkill);
 
 export default col;
