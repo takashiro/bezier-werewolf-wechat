2657a15712c932ddace6cdc8442c7861a6126b9d
Kazuichi Takashiro <takashiro@qq.com>
2020-11-19 22:29:09 +0800

    Add class Minion

---
+++ game/collection/standard/Minion.ts
@@ -0,0 +1,5 @@
+import TargetlessSkill from '../TargetlessSkill';
+
+export default class Minion extends TargetlessSkill {
+	protected buttonLabel = '暗中观察';
+}
--- game/collection/standard/index.ts
+++ game/collection/standard/index.ts
@@ -5,13 +5,15 @@ import TargetlessSkill from '../TargetlessSkill';
 import SinglePlayerSkill from '../SinglePlayerSkill';
 import TwoPlayerSkill from '../TwoPlayerSkill';
 import SingleCardSkill from '../SingleCardSkill';
-import Seer from './Seer';
+
 import Werewolf from './Werewolf';
+import Seer from './Seer';
+import Minion from './Minion';
 
 const col = new Collection('standard');
 col.add(Role.Werewolf, Werewolf);
 col.add(Role.Seer, Seer);
-col.add(Role.Minion, TargetlessSkill);
+col.add(Role.Minion, Minion);
 col.add(Role.Troublemaker, TwoPlayerSkill);
 col.add(Role.Robber, SinglePlayerSkill);
 col.add(Role.Drunk, SingleCardSkill);
