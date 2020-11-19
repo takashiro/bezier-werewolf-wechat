cb67886ecce8652acc7fdf0875d3b5059cb6c27a
Kazuichi Takashiro <takashiro@qq.com>
2020-11-19 22:45:08 +0800

    Add class ParanormalInvestigator

---
+++ game/collection/daybreak/ParanormalInvestigator.ts
@@ -0,0 +1,5 @@
+import SinglePlayerSkill from '../SinglePlayerSkill';
+
+export default class ParanormalInvestigator extends SinglePlayerSkill {
+	protected buttonLabel = '探索领域';
+}
--- game/collection/daybreak/index.ts
+++ game/collection/daybreak/index.ts
@@ -4,15 +4,15 @@ import Collection from '../Collection';
 
 import Werewolf from '../standard/Werewolf';
 import AlphaWolf from './AlphaWolf';
-import TwoPlayerSkill from '../TwoPlayerSkill';
 import WitchView from './WitchView';
 import WitchExchange from './WitchExchange';
 import MysticWolf from './MysticWolf';
+import ParanormalInvestigator from './ParanormalInvestigator';
 
 const col = new Collection('daybreak');
 col.add(Role.Witch, WitchView, WitchExchange);
 col.add(Role.AlphaWolf, Werewolf, AlphaWolf);
 col.add(Role.MysticWolf, Werewolf, MysticWolf);
-col.add(Role.ParanormalInvestigator, TwoPlayerSkill);
+col.add(Role.ParanormalInvestigator, ParanormalInvestigator, ParanormalInvestigator);
 
 export default col;
