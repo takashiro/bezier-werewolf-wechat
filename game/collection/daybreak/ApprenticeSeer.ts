029f6734c6a2d55769e4e68352483960244b02d7
Kazuichi Takashiro <takashiro@qq.com>
2020-12-06 21:57:44 +0800

    Add Apprentice Seer

---
+++ game/collection/daybreak/ApprenticeSeer.ts
@@ -0,0 +1,5 @@
+import SingleCardSkill from '../SingleCardSkill';
+
+export default class ApprenticeSeer extends SingleCardSkill {
+	protected buttonLabel = '预知未来';
+}
--- game/collection/daybreak/index.ts
+++ game/collection/daybreak/index.ts
@@ -4,15 +4,17 @@ import Collection from '../Collection';
 
 import Werewolf from '../standard/Werewolf';
 import AlphaWolf from './AlphaWolf';
-import WitchView from './WitchView';
-import WitchExchange from './WitchExchange';
+import ApprenticeSeer from './ApprenticeSeer';
 import MysticWolf from './MysticWolf';
 import ParanormalInvestigator from './ParanormalInvestigator';
+import WitchView from './WitchView';
+import WitchExchange from './WitchExchange';
 
 const col = new Collection('daybreak');
-col.add(Role.Witch, WitchView, WitchExchange);
 col.add(Role.AlphaWolf, Werewolf, AlphaWolf);
+col.add(Role.ApprenticeSeer, ApprenticeSeer);
 col.add(Role.MysticWolf, Werewolf, MysticWolf);
 col.add(Role.ParanormalInvestigator, ParanormalInvestigator, ParanormalInvestigator);
+col.add(Role.Witch, WitchView, WitchExchange);
 
 export default col;
