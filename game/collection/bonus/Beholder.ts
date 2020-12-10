f649ea9011dd9e6ca379c1ccd92f7441f85a2b4d
Kazuichi Takashiro <takashiro@qq.com>
2020-12-10 21:26:11 +0800

    Add Beholder

---
+++ game/collection/bonus/Beholder.ts
@@ -0,0 +1,5 @@
+import TargetlessSkill from '../TargetlessSkill';
+
+export default class Beholder extends TargetlessSkill {
+	protected buttonLabel = '预知未来';
+}
--- game/collection/bonus/index.ts
+++ game/collection/bonus/index.ts
@@ -2,10 +2,12 @@ import { Role } from '@bezier/werewolf-core';
 import Collection from '../Collection';
 
 import ApprenticeTanner from './ApprenticeTanner';
+import Beholder from './Beholder';
 import Squire from './Squire';
 
 const col = new Collection('bonus');
 col.add(Role.ApprenticeTanner, ApprenticeTanner);
+col.add(Role.Beholder, Beholder);
 col.add(Role.Squire, Squire);
 
 export default col;
