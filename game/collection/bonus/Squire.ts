eaf601d245aae32d51a618da7e61480a74fca2e8
Kazuichi Takashiro <takashiro@qq.com>
2020-12-10 21:06:02 +0800

    Add Squire

---
+++ game/collection/bonus/Squire.ts
@@ -0,0 +1,5 @@
+import TargetlessSkill from '../TargetlessSkill';
+
+export default class Squire extends TargetlessSkill {
+	protected buttonLabel = '暗中观察';
+}
--- game/collection/bonus/index.ts
+++ game/collection/bonus/index.ts
@@ -2,8 +2,10 @@ import { Role } from '@bezier/werewolf-core';
 import Collection from '../Collection';
 
 import ApprenticeTanner from './ApprenticeTanner';
+import Squire from './Squire';
 
 const col = new Collection('bonus');
 col.add(Role.ApprenticeTanner, ApprenticeTanner);
+col.add(Role.Squire, Squire);
 
 export default col;
