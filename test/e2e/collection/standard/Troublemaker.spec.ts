ff53b825161e2da8a591ca03998cf7a2dab80e90
Kazuichi Takashiro <takashiro@qq.com>
2021-03-14 20:35:57 +0800

    Add e2e tests against Troublemaker

---
+++ test/e2e/collection/standard/Troublemaker.spec.ts
@@ -0,0 +1,93 @@
+import { Role } from '@bezier/werewolf-core';
+
+import GameBoard from '../../GameBoard';
+import { lobby } from '../../Lobby';
+import Room from '../../Room';
+
+let room: Room;
+let board: GameBoard;
+
+beforeAll(async () => {
+	await lobby.connect();
+}, 60000);
+
+afterAll(async () => {
+	await lobby.disconnect();
+});
+
+it('returns to lobby', async () => {
+	await lobby.prepare();
+}, 60000);
+
+it('enters the room', async () => {
+	room = await lobby.createRoom([
+		Role.Villager,
+		Role.Werewolf,
+		Role.Tanner,
+		Role.Troublemaker,
+		Role.Hunter,
+		Role.Prince,
+	]);
+});
+
+it('takes Seat 1', async () => {
+	await room.takeSeat(1);
+});
+
+it('opens a game board', async () => {
+	board = await room.getBoard();
+});
+
+it('is a troublemaker', async () => {
+	const [player] = await board.getPlayers();
+	expect(await player.text()).toBe('捣蛋鬼');
+});
+
+it('exchanges 2 players', async () => {
+	const players = await board.getPlayers();
+	expect(players.length).toBe(3);
+	await players[1].tap();
+	await players[2].tap();
+	await board.submit();
+});
+
+it('simulates other players', async () => {
+	const hunter = room.createPlayer(2);
+	await hunter.takeSeat();
+	await hunter.invokeSkill();
+
+	const prince = room.createPlayer(3);
+	await prince.takeSeat();
+	await prince.invokeSkill();
+
+	await hunter.vote(3);
+	await prince.vote(2);
+}, 60000);
+
+it('waits for vote button', async () => {
+	await board.waitForButton('投票');
+});
+
+it('votes for player 2', async () => {
+	const players = await board.getPlayers();
+	const target = players[1];
+	await target.tap();
+	await board.submit();
+});
+
+it('views vote results', async () => {
+	await board.waitForButton('投票结果');
+	await board.submit();
+	await board.waitForVoteResult();
+});
+
+it('verifies skill effects', async () => {
+	const players = await board.getPlayers();
+	expect(await players[0].text()).toBe('捣蛋鬼');
+	expect(await players[1].text()).toBe('王子');
+	expect(await players[2].text()).toBe('猎人');
+});
+
+it('exits the room', async () => {
+	await room.exit();
+});
