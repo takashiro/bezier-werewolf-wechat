ba3bbd9bc4b0324d5edd778de9d3d53028202beb
Kazuichi Takashiro <takashiro@qq.com>
2021-03-14 20:24:50 +0800

    Verify skill effects of a witch

--- test/e2e/collection/daybreak/Witch.spec.ts
+++ test/e2e/collection/daybreak/Witch.spec.ts
@@ -23,11 +23,11 @@ it('returns to lobby', async () => {
 it('enters the room', async () => {
 	room = await lobby.createRoom([
 		Role.Villager,
-		Role.Prince,
+		Role.Werewolf,
 		Role.Tanner,
 		Role.Witch,
 		Role.Hunter,
-		Role.Cursed,
+		Role.Prince,
 	]);
 });
 
@@ -52,11 +52,11 @@ it('chooses one center card', async () => {
 	await board.submit();
 });
 
-it('sees a prince', async () => {
+it('sees a werewolf', async () => {
 	await waitUntil(async () => {
 		const cards = await board.getCards();
 		const text = await cards[1].text();
-		return text === '王子';
+		return text === '狼人';
 	});
 });
 
@@ -72,8 +72,45 @@ it('exchanges the player with the prince', async () => {
 	await waitUntil(async () => {
 		const players = await board.getPlayers();
 		const text = await players[2].text();
-		return text === '王子';
+		return text === '狼人';
 	});
+});
+
+it('simulates other players', async () => {
+	const hunter = room.createPlayer(2);
+	await hunter.takeSeat();
+	await hunter.invokeSkill();
+
+	const prince = room.createPlayer(3);
+	await prince.takeSeat();
+	await prince.invokeSkill();
+
+	await hunter.vote(3);
+	await prince.vote(2);
+}, 60000);
+
+it('waits for vote button', async () => {
+	await board.waitForButton('投票');
+});
+
+it('votes for player 2', async () => {
+	const players = await board.getPlayers();
+	const target = players[1];
+	await target.tap();
+	await board.submit();
+});
+
+it('views vote results', async () => {
+	await board.waitForButton('投票结果');
+	await board.submit();
+	await board.waitForVoteResult();
+});
+
+it('verifies skill effects', async () => {
+	const players = await board.getPlayers();
+	expect(await players[0].text()).toBe('女巫');
+	expect(await players[1].text()).toBe('猎人');
+	expect(await players[2].text()).toBe('狼人');
 });
 
 it('exits the room', async () => {
