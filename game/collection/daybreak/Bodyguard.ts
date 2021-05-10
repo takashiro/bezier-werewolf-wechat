78812254714fa1b616547e658458d91355a3ef42
Kazuichi Takashiro <takashiro@qq.com>
2021-05-10 17:04:19 +0800

    Update description of Bodyguard

--- game/collection/daybreak/Bodyguard.ts
+++ game/collection/daybreak/Bodyguard.ts
@@ -4,8 +4,8 @@ import CollectionEntry from '../CollectionEntry';
 
 const bodyBuard: CollectionEntry = {
 	role: Role.Bodyguard,
-	name: '保镖',
-	description: '一觉醒来，你发现村里为了昨晚的狼人袭击事件互相猜忌，但你认为暗中保护好人更重要。公投时，被保镖选择的玩家不会出局。',
+	name: '守卫',
+	description: '比起找到狼人，你认为暗中保护好人更重要。公投时，你的票会保护目标玩家，该玩家不会出局。若你保护的玩家票数最高，则票数第二（至少1票）的玩家出局。',
 };
 
 export default bodyBuard;
