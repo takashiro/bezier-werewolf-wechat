3f1dccabee6120e3866c8c8aa5ab803315400ca4
Kazuichi Takashiro <takashiro@qq.com>
2021-05-10 14:35:22 +0800

    Correct the description of the Cursed

--- game/collection/bonus/Cursed.ts
+++ game/collection/bonus/Cursed.ts
@@ -3,8 +3,8 @@ import CollectionEntry from '../CollectionEntry';
 
 const cursed: CollectionEntry = {
 	role: Role.Cursed,
-	name: '神弃者',
-	description: '由于猎杀过多狼人，你受到了狼群的诅咒。如果最后没有狼人投票给你，你将会变身为狼人，加入狼人阵营。',
+	name: '被诅咒者',
+	description: '由于猎杀过多狼人，你受到了狼群的诅咒。如果最后至少1名狼人投票给你，你加入狼人阵营。',
 };
 
 export default cursed;
--- game/collection/standard/Hunter.ts
+++ game/collection/standard/Hunter.ts
@@ -4,7 +4,7 @@ import CollectionEntry from '../CollectionEntry';
 const hunter: CollectionEntry = {
 	role: Role.Hunter,
 	name: '猎人',
-	description: '你是村庄里的猎人。当你被公投出局时，所有投票给你的玩家都同时出局。如果有狼人出局，则村民阵营胜利。',
+	description: '你是村庄里的猎人。当你被公投出局时，所有投票给你的玩家都同时出局。其中如果有狼人出局，则村民阵营胜利。',
 };
 
 export default hunter;
